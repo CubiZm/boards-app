98028c6486381a8e53b0dd33dbf2ee8779f6482c
CubiZm <karelense@gmail.com>
2019-09-15 16:23:02 +0300

    Updates

---
+++ precache-manifest.b97b044c9b50b50252b71fe7d63ba81e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6408d1adf95aed0db8993986465e43",
+    "url": "/boards-app/index.html"
+  },
+  {
+    "revision": "2a5bec045757e7b54947",
+    "url": "/boards-app/static/css/main.1086f6b8.chunk.css"
+  },
+  {
+    "revision": "7a850fde7630c131c52d",
+    "url": "/boards-app/static/js/2.1ac8bf2e.chunk.js"
+  },
+  {
+    "revision": "2a5bec045757e7b54947",
+    "url": "/boards-app/static/js/main.8eaea142.chunk.js"
+  },
+  {
+    "revision": "bd7fac86bbfd8bc07310",
+    "url": "/boards-app/static/js/runtime~main.4b258751.js"
+  },
+  {
+    "revision": "b606524308899c08eed65f51f0606eab",
+    "url": "/boards-app/static/media/arrow.b6065243.svg"
+  },
+  {
+    "revision": "f6f012d7ae4b1c001ae8e66e43f90ea6",
+    "url": "/boards-app/static/media/logo.f6f012d7.svg"
+  },
+  {
+    "revision": "48aa7ad98beedc63d82925f45fd8e72c",
+    "url": "/boards-app/static/media/opensans.48aa7ad9.woff2"
+  },
+  {
+    "revision": "811c8baeca3bb3329488f729979c1fbc",
+    "url": "/boards-app/static/media/opensans.811c8bae.woff"
+  },
+  {
+    "revision": "299b3be898e809232985246037e615b9",
+    "url": "/boards-app/static/media/opensanssemibold.299b3be8.woff"
+  },
+  {
+    "revision": "86dd4197bc664e69cc2dcba53bd2e6bc",
+    "url": "/boards-app/static/media/opensanssemibold.86dd4197.woff2"
+  },
+  {
+    "revision": "50fd70b98a1c6a3175baf51fdc4cda30",
+    "url": "/boards-app/static/media/settigns-icon.50fd70b9.svg"
+  },
+  {
+    "revision": "42a40d48c80fe3f477135e206daa621d",
+    "url": "/boards-app/static/media/user-avatar.42a40d48.svg"
+  },
+  {
+    "revision": "0fe516fe8919c7228dd7c1aa2fa0d509",
+    "url": "/boards-app/static/media/user-icon.0fe516fe.svg"
+  }
+]);
